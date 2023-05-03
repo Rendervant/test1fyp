4e1db095d2a28232c673f86fd76279fa038d9d1e
blackbranch <98684948+blackbranch@users.noreply.github.com>
2023-05-03 17:51:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,33 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  // Handle the form submission
+  $username = $_POST['username'];
+  // Do something with the username, such as authenticate the user
+  // ...
+  // Redirect the user to another page after successful authentication
+  header('Location: welcome.php');
+  exit;
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Login Form</title>
+  <title>Login</title>
 </head>
 <body>
 
-	<h2>Login</h2>
+  <h2>Login</h2>
 
-	<form action="login.php" method="post">
-		<label for="username">Username:</label>
-		<input type="text" id="username" name="username" required><br><br>
+  <form method="post">
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" required><br><br>
 
-		<label for="password">Password:</label>
-		<input type="password" id="password" name="password" required><br><br>
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required><br><br>
 
-		<input type="submit" value="Login">
-	</form>
+    <input type="submit" value="Login">
+  </form>
 
 </body>
 </html>
-
